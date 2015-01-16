f08ff81c44c8f330c94322817ebde47c0540ebc1
Lei Lei <leilei@192-195-82-204.PUBLIC.monkeybrains.net>
2015-01-16 02:14:11 -0800

    Implement one non-scroll screen for tip calculator.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var scSwitch: UISwitch!
+    let tipPercentages = [0.18, 0.2, 0.22]
+    let extraPercentages = [0, 0.05, 0.1]
+    let nf = NSNumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.grayColor()
+        totalLabel.text="$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onServiceCharge(sender: AnyObject) {
+        nf.numberStyle = .PercentStyle
+        if scSwitch.on{
+            for (var i=0; i<tipControl.numberOfSegments; i++)
+            {
+                tipControl.setTitle(nf.stringFromNumber(extraPercentages[i]), forSegmentAtIndex: i)
+            }
+        }
+        else{
+            for (var i=0; i<tipControl.numberOfSegments; i++)
+            {
+                tipControl.setTitle(nf.stringFromNumber(tipPercentages[i]), forSegmentAtIndex: i)
+            }
+        }
+        onEditingChange(sender)
+    }
+    
+    @IBAction func onEditingChange(sender: AnyObject) {
+        
+        var tipPrecentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        if scSwitch.on{
+            tipPrecentage = extraPercentages[tipControl.selectedSegmentIndex]
+        }
+        else{
+            tipPrecentage = tipPercentages[tipControl.selectedSegmentIndex]
+        }
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPrecentage
+        var total = billAmount + tip
+        
+        tipLabel.text="$\(tip)"
+        totalLabel.text="$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
